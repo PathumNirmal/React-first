b36bdd73fc194caa6ca35c28acb038bb1df9f3c9
PathumNirmal <pathum.nirmal6@gmail.com>
2023-05-22 14:00:27 +0530

    component creating ways

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 // react component kyala pennanna meka capital walin start karanwa.
-function Greeting() {
-  return <h4>This is pathum and this is my first component</h4>;
-}
+// function Greeting() {
+//   return <h4>This is pathum and this is my first component</h4>;
+// }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello World");
+// };
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1>hello world</h1>
+//     </div>
+//   );
+// }
+
+const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "hello world")
+  );
+};
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
